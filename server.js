cc51633fa6be31d8e6a136e8a0c3cab3dfaac450
Dennis Wang <dennis.wang@saic.com>
2021-07-21 17:48:36 -0700

    added password

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
+
 const app = express();
 
-app.use(express.static('.'));
 
-app.get('/', function(reqeust, response){
+app.get('/', function(request, response){
+    const reject = () => {
+        response.setHeader('www-authenticate', 'Basic')
+        response.sendStatus(401)
+    }
+
+    const authorization = request.headers.authorization
+
+    if(!authorization) {
+    return reject()
+    }
+
+    const [username, password] = Buffer.from(authorization.replace('Basic ', ''), 'base64').toString().split(':')
+
+    if(! (username === 'saic' && password === 'babyyodansdl')) {
+    return reject()
+    }
+    
+    
     response.sendFile(__dirname + '/index.html')
 });
+
+app.use(express.static('.'));
 
 const listener = app.listen(process.env.PORT, function() {
     console.log('Your app is listening on port ' + listener.address().port)
 });
+
+
